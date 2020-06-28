19096122568c4d8b93c0b2a2eb6d8f98000fef51
Erison <erisoncoliveirant@gmail.com>
2020-06-28 18:00:32 -0400

    First commit

---
+++ server/src/routes/users.routes.ts
@@ -0,0 +1,42 @@
+import { Router, response } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
+import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
+import ensureAutheticated from '../middlewares/ensureAuthenticated';
+
+const usersRoutes = Router();
+const upload = multer(uploadConfig);
+
+usersRoutes.post('/', async (request, response) => {
+  const { name, email, password } = request.body;
+
+  const createUser = new CreateUserService();
+
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
+
+  delete user.password;
+
+  return response.json(user);
+});
+
+usersRoutes.patch('/avatar', ensureAutheticated, upload.single('avatar'), async (request, response) => {
+  const updateUSerAvatar = new UpdateUserAvatarService();
+
+  const user = await updateUSerAvatar.execute({
+    user_id: request.user.id,
+    avatarFilename: request.file.filename,
+  });
+
+  delete user.password;
+
+  return response.json(user);
+});
+
+export default usersRoutes;
